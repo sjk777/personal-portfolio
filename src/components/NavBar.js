4bba1f1b7be542fefcf1a4b4a7bb9f85a4e60d0e
Sky K <skyj.kim7@gmail.com>
2023-02-27 10:00:27 -0800

    added navicons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,9 @@
 import {useState, useEffect} from "react";
 import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/logo.svg';
-import navIcon2 from '../assets/img/logo.svg';
-import navIcon3 from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 
 export const NavBar = () =>{
@@ -31,7 +31,7 @@ export const NavBar = () =>{
         <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
         <Container>
           <Navbar.Brand href="#home">
-            <img src={} alt= "Logo"/>
+            <img src={logo} alt= "Logo"/>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span clasName="navbar-toggler-icon"></span>
@@ -44,9 +44,9 @@ export const NavBar = () =>{
             </Nav>
             <span className="navbar-text">
                 <div className="social-icon">
-                    <a href="#"><img src={} alt=""/> </a>
-                    <a href="#"><img src={} alt=""/> </a>
-                    <a href="#"><img src={} alt=""/> </a>
+                    <a href="#"><img src={navIcon1} alt=""/> </a>
+                    <a href="#"><img src={navIcon2} alt=""/> </a>
+                    <a href="#"><img src={navIcon3} alt=""/> </a>
                 </div>
                 <button className="vvd" onClick={() => console.log('connect')}>
                     <span>Let's Connect</span>
