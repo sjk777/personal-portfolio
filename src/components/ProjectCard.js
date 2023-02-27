7816b1298992dbff614a3dddde61dfa3cea65174
Sky K <skyj.kim7@gmail.com>
2023-02-27 13:07:30 -0800

    added project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,3 +1,5 @@
+import {Col } from "react-bootstrap";
+
 export const ProjectCard = ({title, description, imgUrl}) =>{
     return(
         <Col sm={6} md={4}>
@@ -6,6 +8,7 @@ export const ProjectCard = ({title, description, imgUrl}) =>{
             </div>
             <div className="project-txt">
                 <h4>{title}</h4>
+                <span>{description}</span>
             </div>
         </Col>
     )
